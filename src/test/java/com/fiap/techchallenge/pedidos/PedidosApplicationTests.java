a9e6d2ebe120e4a8b48fe0bb2f2863f0822af198
Douglas Gomes <douglas.gomes@perinity.com>
2024-11-26 23:44:10 -0300

    removido teste da classe principal do spring

--- src/test/java/com/fiap/techchallenge/pedidos/PedidosApplicationTests.java
+++ src/test/java/com/fiap/techchallenge/pedidos/PedidosApplicationTests.java
@@ -3,10 +3,10 @@ package com.fiap.techchallenge.pedidos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PedidosApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
