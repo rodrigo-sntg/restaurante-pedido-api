8b99a4311e8432babcd112161f17bd61765c326d
Douglas Gomes <douglas.gomes@perinity.com>
2024-12-01 01:25:17 -0300

    não deve dar push na main

--- src/main/java/com/fiap/techchallenge/pedidos/infraestruture/web/externalapi/PagamentoExternalGatewayImpl.java
+++ src/main/java/com/fiap/techchallenge/pedidos/infraestruture/web/externalapi/PagamentoExternalGatewayImpl.java
@@ -7,12 +7,15 @@ import com.fiap.techchallenge.pedidos.application.gateway.PagamentoExternalGatew
 import com.fiap.techchallenge.pedidos.domain.model.Pedido;
 import com.fiap.techchallenge.pedidos.infraestruture.configuration.ExternalApiRoutes;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PagamentoExternalGatewayImpl implements PagamentoExternalGateway {
 	private final ExternalApiRoutes externalApiRoutes;
 	private final RestTemplate restTemplate;
@@ -20,7 +23,7 @@ public class PagamentoExternalGatewayImpl implements PagamentoExternalGateway {
 	@Override
 	public CheckoutDTO fazerCheckout(Pedido pedido) {
 		String url = externalApiRoutes.getPagamentoApiUrl() + "/api/checkouts";
-
+		log.info("Fazendo checkout na API externa: {}", url);
 		var itens = pedido.getItens()
 				.stream()
 				.map(item -> ItemPedidoCheckoutDTO.builder()
@@ -39,7 +42,10 @@ public class PagamentoExternalGatewayImpl implements PagamentoExternalGateway {
 
 		ResponseEntity<CheckoutDTO> response = restTemplate.postForEntity(url, pedidoDTO,
 				CheckoutDTO.class);
-		return response.getBody();
+		if(response.getStatusCode() == HttpStatus.OK) {
+			return response.getBody();
+		}
+		throw new RuntimeException("Erro ao fazer checkout");
 	}
 
 	private int converteParaCentavos(Double value) {
